e8dd6fd8c82586452a6d9007915ae851d0059111
xun_yu <xun_yu@hunliji.com>
2018-11-22 15:16:54 +0800

    多进程支持

--- index.php
+++ index.php
@@ -1,35 +1,70 @@
 <?php
+
 /**
  * cli运行环境
  * @author xunyu
  * @date 2018-11-21
  */
 include_once 'model/dbModel.php';
-$runType=0;
+include_once 'common/common.php';
+$runType = 0;
 
 //判断运行环境
-if(!empty($argv[1])){
-    if($argv[1]=="test"){
+if (!empty($argv[1])) {
+    if ($argv[1] == "test") {
         //测试环境
-        $runType=1;
+        $runType = 1;
     }
 }
 
 //加载配置文件
-if($runType==1){
+if ($runType == 1) {
     include_once 'conf/conf_test.php';
 } else {
     include_once 'conf/conf.php';
 }
 
-
 //循环执行任务
 while (true) {
-    $con=dbModel::connect(conf::$DB['DB_HOST'], conf::$DB['DB_USERNAME'], conf::$DB['DB_PASSWORD'], conf::$DB['DB_DATABASE'],conf::$DB['DB_PORT']);
-    $out=$con->query("SELECT a.*,b.name FROM user_influence a left join user b on a.user_id=b.id order by id desc limit 10");
-    $con->close();
-    while($row = mysqli_fetch_assoc($out)){
-        echo $row['id'].":".$row['name'];
+    //遍历扫描task文件
+    $fires = my_dir("task/");
+    foreach ($fires as $key => $value) {
+        //剔除接口
+        if ($value == 'taskInterface.php') {
+            unset($fires[$key]);
+            break;
+        }
+    }
+
+    if (!empty($fires)) {
+        foreach ($fires as $key => $value) {
+            include_once 'task/' . $value;
+            $tmp = explode('.', $value);
+
+            //判断执行时间是否已经到了
+            $exec_time = $tmp[0]::getRunTime();
+
+            //判断是否已经执行过了
+            //新建进程执行
+            $pid = pcntl_fork();
+            if ($pid == -1) {
+                die("could not fork");
+            } elseif ($pid) {
+                echo "I'm the Parent $i\n";
+                pcntl_waitpid($pid, $status,WNOHANG);
+                var_dump($status);
+            } else {// 子进程处理
+                $out[] = array(
+                    'taskName' => $tmp[0]::getTaskName(),
+                    'runTime' => $tmp[0]::getRunTime(),
+                    'responsiblePeopleName' => $tmp[0]::getResponsiblePeopleName(),
+                    'out' => $tmp[0]::run()
+                );
+                var_dump($out);
+                exit;
+            }
+            //保存执行结果
+        }
     }
     break;
 }
---
+++ task/userInfluenceRankTask.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once 'taskInterface.php';
+include_once 'model/dbModel.php';
+
+/**
+ * Description of userInfluenceRankTask
+ *
+ * @author hunliji
+ */
+class userInfluenceRankTask implements task {
+
+    public static function getTaskName() {
+        return "影响力排行计算";
+    }
+
+    public static function getRunTime() {
+        return array(
+            'type' => "every_day",
+            'time_interval' => "once",
+            'time' => "12:00:00"
+        );
+    }
+
+    public static function getResponsiblePeopleName() {
+        return "xunyu";
+    }
+
+    public static function run() {
+        $con = dbModel::connect(conf::$DB['DB_HOST'], conf::$DB['DB_USERNAME'], conf::$DB['DB_PASSWORD'], conf::$DB['DB_DATABASE'], conf::$DB['DB_PORT']);
+        $out = $con->query("SELECT a.*,b.name FROM user_influence a left join user b on a.user_id=b.id order by id desc limit 10");
+        $con->close();
+        while ($row = mysqli_fetch_assoc($out)) {
+            echo $row['id'] . ":" . $row['name'];
+        }
+        return 12345;
+    }
+}
