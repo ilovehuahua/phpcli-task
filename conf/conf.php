1ff2c871489ad9309013ffead9a1b5df3ab49bca
xun_yu <xun_yu@hunliji.com>
2018-11-22 10:07:54 +0800

    新增conf

---
+++ conf/conf.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * php任务配置
+ *
+ * @author hunliji
+ */
+class conf {
+    public static $DB=array(
+        "DB_HOST"=>"localhost",
+        "DB_DATABASE"=>"testDB",
+        "DB_PORT"=>3306,
+        "DB_USERNAME"=>"username",
+        "DB_PASSWORD"=>"password"
+    );
+}
