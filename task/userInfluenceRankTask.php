4be32782de3301b12e83891fd50fdb88ef9686f6
xun_yu <xun_yu@hunliji.com>
2018-11-26 11:12:22 +0800

    bugfix

--- task/userInfluenceRankTask.php
+++ task/userInfluenceRankTask.php
@@ -18,7 +18,7 @@ class userInfluenceRankTask implements task {
         return array(
             'type' => "every_day",
             'time_interval' => "once",
-            'time' => "17:30:00"
+            'time' => "02:00:10"
         );
     }
 
